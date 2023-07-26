48b0c2436dabe7407953f305c01dfaec3a7b2b00
mtork <mtork@netflix.com>
2023-07-26 14:00:26 -0700

    timeouts only

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,12 @@ export default defineConfig({
     baseUrl: "http://localhost:5000",
   },
   numTestsKeptInMemory: 1000,
+  requestTimeout:120000,
+  taskTimeout:120000,
+  pageLoadTimeout:120000,
+  defaultCommandTimeout:120000,
+  execTimeout:120000,
+  responseTimeout:120000,
   component: {
     devServer: {
       framework: "create-react-app",
