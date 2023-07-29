9576a76347434a43933b6c96674744557c916ed8
mtork <mtork@netflix.com>
2023-07-29 09:50:48 -0700

    fake contains no test

--- cypress/component/Fake.cy.js
+++ cypress/component/Fake.cy.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-describe("Await browser", ()=>{
-    it("Wait test", ()=>{
-        const syncWait = ms => {
-            const end = Date.now() + ms
-            while (Date.now() < end) continue
-        }
-        
-        syncWait(10000)
-    })
-})
+// describe("Await browser", ()=>{
+//     it("Wait test", ()=>{
+const syncWait = ms => {
+    const end = Date.now() + ms
+    while (Date.now() < end) continue
+}
+
+syncWait(10000)
+//     })
+// })
