364d7be78f882d2bc4ed5527c99704903a294e9c
Michael Torku <72820059+michaeltorku@users.noreply.github.com>
2023-07-31 09:29:52 -0700

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,22 +1,11 @@
-import { defineConfig } from "cypress";
-
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:5000",
-    // specPattern: "./cypress/**/*.cy.{js}"
   },
   numTestsKeptInMemory: 1000,
-  requestTimeout:120000,
-  taskTimeout:120000,
-  pageLoadTimeout:120000,
-  defaultCommandTimeout:120000,
-  execTimeout:120000,
-  responseTimeout:120000,
   component: {
-    // specPattern: "./cypress/**/*.cy.{js}",
     devServer: {
       framework: "create-react-app",
       bundler: "webpack",
     },
   },
-});
