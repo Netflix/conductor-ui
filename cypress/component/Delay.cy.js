7d932886e19215721c255edd74c17bfe1146f43a
mtork <mtork@netflix.com>
2023-07-31 09:52:09 -0700

    no wait

--- cypress/component/Delay.cy.js
+++ cypress/component/Delay.cy.js
@@ -1,10 +1,5 @@
 import React from "react";
-// Synchronous wait to prevent browser time out
+// Placeholder file to prevent browser time out
 // in CI tests (github actions)
 // Should be the first component test (alphabetical order)
-const syncWait = (ms) => {
-  const end = Date.now() + ms;
-  while (Date.now() < end) continue;
-};
 
-syncWait(10000);
