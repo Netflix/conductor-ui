f6733a2afb71641c260e520ff6ed9cd9c3f3a9a2
Jacqueline He <jacquelineh@netflix.com>
2023-08-21 14:04:19 -0700

    fix error

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,6 @@ import DiagramTest from "./pages/kitchensink/DiagramTest";
 import Examples from "./pages/kitchensink/Examples";
 import Gantt from "./pages/kitchensink/Gantt";
 import Workbench from "./pages/workbench/Workbench";
-import RcDockTest from "./pages/kitchensink/RCDockTest";
 import TaskConfiguratorPage from "./pages/kitchensink/TaskConfiguratorPage";
 
 // For rc-dock
@@ -96,7 +95,6 @@ export default function App({
           <Route path="/kitchen/diagram" element={<DiagramTest />} />
           <Route path="/kitchen/examples" element={<Examples />} />
           <Route path="/kitchen/gantt" element={<Gantt />} />
-          <Route path="/kitchen/rcdock" element={<RcDockTest />} />
           <Route
             path="/kitchen/taskConfigurator"
             element={<TaskConfiguratorPage />}
