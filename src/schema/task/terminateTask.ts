29fe5fb576b16afd76927804623606d77ece0cdb
Jacqueline He <jacquelineh@netflix.com>
2023-08-22 14:46:41 -0700

    add terminate task type (buggy)

--- src/schema/task/terminateTask.ts
+++ src/schema/task/terminateTask.ts
@@ -1,4 +1,6 @@
-export const TerminateTaskParameters = [
+import { TerminalTaskConfig, TerminateTaskConfig } from "../../types/workflowDef";
+
+export const terminateTaskParameters = [
   {
     id: 0,
     key: "name",
@@ -54,4 +56,49 @@ export const TerminateTaskParameters = [
     required: false,
     type: "int",
   },
+  {
+    id: 7,
+    key: "terminationStatus",
+    value: "completed",
+    changed: false,
+    required: true,
+    type: "string",
+  },
+  {
+    id: 8,
+    key: "terminationReasion",
+    value: "",
+    changed: false,
+    required: false,
+    type: "string",
+  },
 ];
+
+
+export function createTerminateTaskParams(taskReferenceName) {
+  let taskParams = {};
+  let inputParameters = {};
+
+  taskParams["type"] = "TERMINATE";
+
+  terminateTaskParameters.forEach((parameter) => {
+    // Only expose fields that are marked as required
+    if (parameter.required === true) {
+      // Sets the value as the respective input if the key matches "name" or "taskReferenceName", otherwise, uses the default value
+      if (parameter.key === "name") {
+        taskParams[parameter.key] = taskReferenceName;
+      } else if (parameter.key === "taskReferenceName") {
+        taskParams[parameter.key] = taskReferenceName;
+      } else if (parameter.key === "terminationStatus") {
+        inputParameters[parameter.key] = parameter.value;
+      } else {
+        taskParams[parameter.key] = parameter.value;
+      }
+    }
+  });
+  taskParams["inputParameters"] = inputParameters;
+
+  return taskParams as TerminateTaskConfig;
+}
+
+
